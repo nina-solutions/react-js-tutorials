1de43f4e6eaa1d7883f038df4bcc54107a14b22d
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2018-03-07 00:48:29 +0100

    playing with thunk or promises for anync actions

--- 4-redux/src/js/app.js
+++ 4-redux/src/js/app.js
@@ -1,14 +1,14 @@
 import { applyMiddleware, createStore, combineReducers } from "redux";
+import axios from "axios";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
 
-const logger = (store) => (next) => (action) => {
-    console.log("action fired ", action);
-    next(action)
-};
 const error = (store) => (next) => (action) => {
     try {
         next(action)
     } catch(e) {
-        console.log("err ", e);
+        console.log("err ", e.message);
     }
 };
 
@@ -30,23 +30,54 @@ const userReducer = function (state={}, action) {
     return state;
 };
 
-const tweetsReducer = function (state={}, action) {
+const apiReducer = function (state={}, action) {
+    switch (action.type) {
+        case "FETCH_USERS_START": {
+            state = {...state, fetching: true };
+            break;
+        }
+        case "RECEIVE_USERS": {
+            state = {...state, fetching: false, fetched: true, users: action.payload };
+            break;
+        }
+        case "FETCH_USERS_ERR": {
+            state = {...state, error: action.payload };
+            break;
+        }
+
+        case "PROMISE_FETCH_PENDING": {
+            state = {...state, fetching: true };
+            break;
+        }
+        case "PROMISE_FETCH_FULFILLED": {
+            state = {...state, fetching: false, fetched: true, users: action.payload };
+            break;
+        }
+        case "PROMISE_FETCH_REJECTED": {
+            state = {...state, error: action.payload };
+        }
+    }
     return state;
 };
 
 const reducers = combineReducers({
     user: userReducer,
-    tweets: tweetsReducer
+    api: apiReducer
 });
 
-const middleware = applyMiddleware(logger, error);
+const middleware = applyMiddleware(promise(), thunk, logger(), error);
 
 const store = createStore(reducers, {
     user: {
         name: 'KAroly',
         age: 33,
     },
-    tweets: []
+    api: {
+        fetching: false,
+        fetched: false,
+        error: null,
+        users: []
+    }
 }, middleware);
 
 store.subscribe(() => {
@@ -57,4 +88,17 @@ store.dispatch({type: "CHANGE_NAME", payload: "Karoly Albert"});
 store.dispatch({type: "CHANGE_AGE", payload: 25});
 store.dispatch({type: "ulo", payload: 25});
 
+store.dispatch((dispatch) => {
+    dispatch({type: "FETCH_USERS_START"});
+    axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(response => dispatch({type: "RECEIVE_USERS", payload: response.data}))
+        .catch(err => dispatch({type: "FETCH_USERS_ERR", payload: err}));
+    axios.get("https://jsonplaceholder.typicode.com/XXX")
+        .then(response => dispatch({type: "RECEIVE_USERS", payload: response.data}))
+        .catch(err => dispatch({type: "FETCH_USERS_ERR", payload: err}))
+});
 
+store.dispatch({
+    type: "PROMISE_FETCH",
+    payload: axios.get("https://jsonplaceholder.typicode.com/users")
+});
